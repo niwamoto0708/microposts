0a7775e0129d45a9b8b94ea0c4dda0717e3a6ee7
nao <rb.naoyuki@gmail.com>
2017-01-04 17:21:49 +0000

    Kadai 7 Login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
   
   def show
     @user = User.find(params[:id])
@@ -18,10 +20,29 @@ class UsersController < ApplicationController
     end
   end
   
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+  
   private
   
   def user_params
-    params.require(:user).permit(:name, :email, :password, :passwrod_confirmation)
+    params.require(:user).permit(:name, :email, :password, :passwrod_confirmation, :profile, :area, :HomePage, :birth)
+  end
+  
+  def correct_user
+      @user = User.find(params[:id])
+      redirect_to root_path unless(current_user == @user)
   end
   
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,10 @@ class User < ActiveRecord::Base
     validates :email, presence: true, length: { maximum: 255 },
                         format: { with: VALID_EMAIL_REGEX },
                         uniqueness: { case_sensitive: false }
+    validates :profile, presence: true, length: { maximum: 255 }, allow_nil: true
+    validates :area, presence: true, length: { maximum: 20 }, allow_nil: true
+    validates :HomePage, presence: true, length: { maximum: 100 }, allow_nil: true
+    validates :birth, presence: true, allow_nil: true
     has_secure_password
+    validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 end
